7f9e98dfae099e30d81a4f4b8435cf84d74f37a4
刘芸杉 <alexanderde1591@gmail.com>
2016-08-15 17:14:53 +0800

    pagination
    pagination is useful now

--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -1,10 +1,11 @@
 <template>
-	<div class="swiper-container" data-space-between="0">
+	<div class="swiper-container">
 		<div class="swiper-wrapper">
 			<div class="swiper-slide" v-for="item in swipes">
 				<img width="100%" height="150" v-bind:src="item.src" />
 			</div>
 		</div>
+		<div class="swiper-pagination" style="background:none;"></div>
 	</div>
 </template>
 
@@ -20,8 +21,13 @@
 		methods: {
 			getSwipes(){
 				var _self = this
-				$.get("/static/data/swipes.json",{"params":250},function(data){
-					if(data.code==1)_self.swipes = data.banner
+				$.get("/static/data/swipes.json", {"params": 250}, function(data){
+					if(data.code==1){
+						_self.swipes = data.banner;
+						setTimeout(function(){
+							$.init();
+						});
+					}
 				},"json")
 			}
 		},
